a465c57c880027824823f50bd30254cf36e68ede
James Foran <james@flexion.tech>
2020-08-31 22:09:37 +1000

    adding utils

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,18 +1,22 @@
+const utils = require('utils')
+
 exports.handler = (event, context, callback) => {
   try {
     console.log('hello-world started')
+    const message = {}
+    message.hello = utils.hello()
+    message.goodbye = utils.goodbye()
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify('hello-world'),
+      body: JSON.stringify(message),
     }
   } catch (error) {
-    const message = 'testing errors'
-    console.log(message)
+    console.log(error)
     return {
-      statusCode: '400',
+      statusCode: 500,
       headers: { 'Content-Type': 'Text' },
-      body: message,
+      body: JSON.stringify(error),
     }
   } finally {
     console.log('and we are done')
---
+++ functions/utils/src/index.js
@@ -0,0 +1,11 @@
+module.exports.hello =  () => {
+  console.log("hello function")
+  console.trace()
+  return "hello from utils"
+}
+
+module.exports.goodbye =  () => {
+  console.log("goodbye function")
+  console.trace()
+  return "goodbye from utils"
+}
